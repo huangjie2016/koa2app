6044b652b516cc01a672b29a1f09cb744c3b0ee3
mooehuangjie <huangjie@mooe-robot.com>
2022-05-28 18:28:34 +0800

    feat:first-commit

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ onerror(app)
 
 // middlewares
 app.use(bodyparser({
-  enableTypes:['json', 'form', 'text']
+  enableTypes: ['json', 'form', 'text']
 }))
 app.use(json())
 app.use(logger())
@@ -26,7 +26,7 @@ app.use(views(__dirname + '/views', {
 
 // logger
 app.use(async (ctx, next) => {
-  const start = new Date()
+  const start = new Date();
   await next()
   const ms = new Date() - start
   console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
